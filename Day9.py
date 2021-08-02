2d87792778b3464b7ca0926cfb6fdfd8c030927d
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-08-02 23:58:56 +0530

    Create Day9.py

---
+++ Day9.py
@@ -0,0 +1,24 @@
+
+n = int(input('Enter the number of integers in the numbers : '))
+numbers = []
+
+print('Enter ',n,' integers')
+for i in range(0,n):
+    numbers.append(int(input()))
+
+
+pos = 0
+for i in range(0,n-1):
+    if(numbers[i] > numbers[pos]):        
+        pos = i
+
+# removing the largest element of the numbers
+numbers.pop(pos)
+
+
+high = 0
+for i in range(0,n-2):
+    if(numbers[i] > numbers[i+1]):        
+        high = numbers[i]
+
+print('The second highest number in the list is : ',high)
